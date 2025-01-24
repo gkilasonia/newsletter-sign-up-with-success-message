d4d9b79269d482e9cdd5ab81a0d5f71ce71a0101
gkilasonia <dgiorjio@gmail.com>
2025-01-24 15:36:06 +0400

    desktop version and active state is done

--- index.js
+++ index.js
@@ -4,9 +4,20 @@ const successMessageSection = document.getElementById(
   "success-message-section"
 );
 const formButton = document.getElementById("form-button");
+const dismissButton = document.getElementById("dismiss-button");
 
-formButton.addEventListener("click", function () {
+formButton.addEventListener("click", function (event) {
+  event.preventDefault();
+  console.log("Form submission prevented!");
   picture.style.display = "none";
   textSectionContainer.style.display = "none";
-  successMessageSection.style.display = "block";
+  successMessageSection.style.display = "flex";
+});
+
+dismissButton.addEventListener("click", function (event) {
+  event.preventDefault();
+  console.log("Form submission prevented!");
+  picture.style.display = "block";
+  textSectionContainer.style.display = "flex";
+  successMessageSection.style.display = "none";
 });
